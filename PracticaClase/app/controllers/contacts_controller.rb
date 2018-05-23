5ce32c96f6bba3c08388fd105f0cd19af48018bf
edgaray <32691733+garayf@users.noreply.github.com>
2018-05-23 15:22:26 -0500

    Add files via upload

---
+++ PracticaClase/app/controllers/contacts_controller.rb
@@ -0,0 +1,12 @@
+class ContactsController < ApplicationController
+    
+    def index
+    
+        @contacts = Contact.all
+        render json: @contacts, status: :ok
+    
+        
+    end
+    
+    
+end
